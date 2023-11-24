a14aae7a33a02255027f3388d06483a682ddb91e
Nasihul Islam Nipun <117701860+nipun-info@users.noreply.github.com>
2023-11-24 10:31:13 +0000

    container dev config

---
+++ 01basicjs/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "nipun@gmail.com"
+var accountPassword = "147896"
+accountCity = "Dinajpur"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity])
